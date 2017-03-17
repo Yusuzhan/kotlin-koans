1cc50d96f3d5612df8d4ce018720ea469e7af812
Yusuzhan <yusuzhan@qq.com>
2017-03-17 10:36:30 +0800

    test 4 passed

--- src/i_introduction/_4_Lambdas/Lambdas.kt
+++ src/i_introduction/_4_Lambdas/Lambdas.kt
@@ -21,7 +21,15 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean {
+    val lambda = { x: Int -> x % 42 }
+    for (i in collection){
+        if (lambda(i) == 0){
+            return true
+        }
+    }
+    return false
+}
 
 
 
