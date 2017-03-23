4877115b2edddfca7153b14c0a13846c5d64564c
Yusuzhan <yusuzhan@qq.com>
2017-03-23 20:46:44 +0800

    Problem 23 solved

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -2,17 +2,21 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 }
 
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    // Return the number of times the given product was ordered.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
+        //customers.fold(0, { count, customer -> count + customer.orders.flatMap { it.products }.filter { it == product }.size })
+        customers.fold(0, { count, customer -> count + customer.orders.flatMap { it.products }.count { it == product } })
+// Return the number of times the given product was ordered.
+// Note: a customer may order the same product for several times.
+// solution 1 : using flatMap
+// return customers.flatMap { it.orders }.flatMap { it.products }.filter { it == product }.size
+
+
